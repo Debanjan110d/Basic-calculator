78d1510f7bfc636ca35a2e362e6657d0c50e59f9
Debanjan110d <debanjangamedu@gmail.com>
2024-06-29 23:33:12 +0530

    All Set

---
+++ script.js
@@ -0,0 +1,20 @@
+//lets Start
+
+const display = document.getElementById("display");
+function appendToDisplay(input) {
+	display.value += input;
+}
+
+function clearDisplay() {
+	display.value = "";
+}
+function calculate() {
+	try {
+		display.value = eval(display.value);
+	} catch (error) {
+		display.value = "Syntax error";
+	}
+}
+function deleteLast() {
+	display.value = display.value.slice(0, -1);
+}
